9fcfd4cf5f0085095861539920592e4837236ccb
N2C <nicolasdechastaing@gmail.com>
2017-10-24 10:01:29 +0200

    add autoload file with PSR-0 standard

---
+++ utilities/autoload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 24/10/2017
+ * Time: 10:00
+ */
+
+function __autoload($className)
+{
+    $folder = "./";
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    include $folder . $fileName;
+}
