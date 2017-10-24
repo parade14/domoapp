f34f008b6b9d544252cf5807836b475c2c07a409
N2C <nicolasdechastaing@gmail.com>
2017-10-24 09:51:05 +0200

    add user and message entities

---
+++ entities/Message.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 24/10/2017
+ * Time: 09:44
+ */
+
+namespace domoapp\Entities;
+
+
+class Message
+{
+
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var integer
+     */
+    protected $subject;
+
+    /**
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * @var integer
+     */
+    protected  $destinataireId;
+
+    /**
+     * @var integer
+     */
+    protected $authorId;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Message
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param int $subject
+     * @return Message
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     * @return Message
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDestinataireId()
+    {
+        return $this->destinataireId;
+    }
+
+    /**
+     * @param int $destinataireId
+     * @return Message
+     */
+    public function setDestinataireId($destinataireId)
+    {
+        $this->destinataireId = $destinataireId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param int $authorId
+     * @return Message
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->authorId = $authorId;
+        return $this;
+    }
+
+
+}
---
+++ entities/User.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 24/10/2017
+ * Time: 09:38
+ */
+
+namespace domoapp\Entities;
+
+
+class User
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string;
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     */
+    protected $lastName;
+
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     */
+    protected $profileType;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProfileType()
+    {
+        return $this->profileType;
+    }
+
+    /**
+     * @param string $profileType
+     * @return User
+     */
+    public function setProfileType($profileType)
+    {
+        $this->profileType = $profileType;
+        return $this;
+    }
+
+
+}
