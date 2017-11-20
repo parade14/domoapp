2f0376c665c2ea01a1585fe39125048957e983f4
N2C <nicolasdechastaing@gmail.com>
2017-11-20 09:40:00 +0100

    creating service handler

---
+++ services/handler/ServiceHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 20/11/2017
+ * Time: 09:26
+ */
+
+namespace domoapp\services\handler;
+
+
+abstract class ServiceHandler implements ServiceHandlerInterface
+{
+
+}
