2b22e7c0e8f94ede0478920eb64cecdabdf18a80
N2C <nicolasdechastaing@gmail.com>
2017-11-20 11:31:30 +0100

    correcting typo word DateTime

---
+++ entities/GroupAccommodation.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * User: quentin
+ * Date: 20/11/2017
+ */
+
+namespace domoapp\Entities;
+
+
+class GroupAccommodation
+{
+
+    /**
+     * @var int
+     */
+    protected $accommodationId;
+
+    /**
+     * @var int
+     */
+    protected $groupId;
+
+
+
+    /**
+     * @return int
+     */
+    public function getAccommodationId()
+    {
+        return $this->accommodationId;
+    }
+
+    /**
+     * @param int $accommodationId
+     * @return GroupAccommodation
+     */
+    public function setAccommodationId($accommodationId)
+    {
+        $this->accommodationId = $accommodationId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroupId()
+    {
+        return $this->groupId;
+    }
+
+    /**
+     * @param int $groupId
+     * @return GroupAccommodation
+     */
+    public function setGroupId($groupId)
+    {
+        $this->groupId = $groupId;
+        return $this;
+    }
+
+
+  
+
+}
