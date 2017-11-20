88b8d0f8ecb523424497ed8253d69e83a175aff4
Quentin Cornier <quco10702@ma-3020-lx>
2017-11-20 11:53:46 +0100

    add user service interface

---
+++ services/user/UserServiceInterface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * User: quentin
+ * Date: 20/11/2017
+ */
+
+namespace domoapp\Services\User;
+
+
+use domoapp\services\handler\ServiceInterface;
+use domoapp\entity\User;
+
+interface UserServiceInterface extends ServiceInterface
+{
+    /**
+     * Add an user in database
+     * @param User $user
+     * @return boolean
+     */
+    public function createUser(User $user);
+
+    /**
+     * Delete an user in database
+     * @param string $idUser
+     * @return boolean
+     */
+    public function deleteUser($idUser);
+
+    /**
+     * Update an user in database with the new value
+     * @param string $idUser
+     * @return boolean
+     */
+    public function updateUser($idUser);
+
+    /**
+     * Return the user with the id in parameter
+     * @param string $idUser
+     * @return User
+     */
+    public function getUser($idUser);
+
+
+
+}
