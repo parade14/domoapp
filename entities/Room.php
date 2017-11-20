edd91d92cbf40c7b61ba51122ee860b676c5b6df
Quentin Cornier <quco10702@ma-3020-lx>
2017-11-20 09:50:22 +0100

    add room entity

---
+++ entities/Room.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * User: quentin
+ * Date: 20/11/2017
+ */
+
+namespace domoapp\Entities;
+
+
+class Room
+{
+
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var int
+     */
+    protected $area;
+
+    /**
+     * @var int
+     */
+    protected $accomodationId;
+
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Room
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Room
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getArea()
+    {
+        return $this->area;
+    }
+
+    /**
+     * @param int $area
+     * @return Room
+     */
+    public function setArea($area)
+    {
+        $this->area = $area;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAccomodationId()
+    {
+        return $this->accomodationId;
+    }
+
+    /**
+     * @param int $accomodationId
+     * @return Room
+     */
+    public function setAccomodationId($accomodationId)
+    {
+        $this->accomodationId = $accomodationId;
+        return $this;
+    }
+
+  
+
+}
