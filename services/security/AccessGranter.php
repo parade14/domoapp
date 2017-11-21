e3bc9b8a1fa7587d9a58a4ce7cc1a5bf6ae44d68
N2C <nicolasdechastaing@gmail.com>
2017-11-21 09:01:16 +0100

    création de accessGranter

---
+++ services/security/AccessGranter.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicol
+ * Date: 21/11/2017
+ * Time: 08:59
+ */
+
+namespace domoapp\services\security;
+
+
+use domoapp\services\HttpFoundation\AccessDeniedException;
+
+class AccessGranter implements AccessGranterInterface
+{
+    public function __construct()
+    {
+
+    }
+
+
+    public function isGranted($role)
+    {
+        // TODO: Implement isGranted() method.
+    }
+
+}
