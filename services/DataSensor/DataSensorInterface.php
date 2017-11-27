bb686e34fb6c2ae8d8735b4f097e5c733ad05e90
Quentin Cornier <quco10702@ma-3020-lx>
2017-11-27 10:57:46 +0100

    add datasensor service

---
+++ services/DataSensor/DataSensorInterface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * User: quentin
+ * Date: 27/11/2017
+ */
+
+namespace Services\DataSensor;
+
+
+interface DataSensorInterface
+{
+
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @param int $id
+     * @return DataSensor
+     */
+    public function setId($id);
+
+    /**
+     * @return int
+     */
+    public function getSensorId();
+
+    /**
+     * @param int $sensorId
+     * @return DataSensor
+     */
+    public function setSensorId($sensorId);
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate();
+
+    /**
+     * @param \DateTime $date
+     * @return DataSensor
+     */
+    public function setDate(\DateTime $date);
+  
+
+}
