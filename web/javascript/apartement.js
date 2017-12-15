641bd7b777bae1803b341c7f0698283200d062a3
parade14 <ppaul.bellamy@gmail.com>
2017-12-15 15:07:44 +0100

    Appartment design modified

---
+++ web/javascript/apartement.js
@@ -0,0 +1,8 @@
+
+
+
+$('.show_hide').click(function () {
+
+// here we toggle show/hide the correct div at the right speed and using which easing effect
+    $('.toggleDiv').slideToggle(1000, 'swing');
+});
