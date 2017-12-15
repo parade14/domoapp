8aa6e9d3eaeaf2ac4f33b9967e65242bb2e2186e
parade14 <ppaul.bellamy@gmail.com>
2017-12-15 15:38:25 +0100

    fixed user-home.php

--- web/html/user/user-home.php
+++ web/html/user/user-home.php
@@ -3,62 +3,53 @@
 
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../../design/css/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../../design/css/ionicons/css/ionicons.min.css">
-        <link rel="stylesheet" href="../../design/css/user-home-style.css" />
-
-
-
-
-
-
+        <link rel="stylesheet" href="../design/css/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../design/css/ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="../design/css/user-home-style.css" />
         <title>User Home</title>
     </head>
 
     <body>
+        <div class="header">
+            <div id="logo">
+                <img src="../design/images/logo_nascop.png" alt="Logo Nascop" />
+            </div>
+            <nav>
+                <ul>
+                    <li><a class="menu_effect" href="#"> Accueil </a> </li>
+                    <li><a class="menu_effect" href="#"> Statistiques </a> </li>
+                    <li><a class="menu_effect" href="#"> Appartements </a> </li>
+                    <li><a class="menu_effect" href="#"> Capteurs </a> </li>
+                    <li><a class="menu_effect menu-right" href="#"> Nous contacter </a> </li>
+                    <li><a class="menu_effect" href="#"> Mon profil </a> </li>
+                    <li><a class="menu_effect" href="#"> Langue </a> </li>
+                </ul>
+            </nav>
+        </div>
 
-
-        <?php include('templates/header.php'); ?>
-
-       
         <section>
+            <article>
                 <div class="informations-wrapper">
-                    <fieldset>
-                        <legend>Actualités</legend>
-                        <ul class="informations-text">
-                            <li>Système d'alarme désactivé</li>
-                            <li>Nascop : nos offres évoluent, soutien de proximité avec notre nouveau chat</li>
-                        </ul>
-                    </fieldset>
+                    <h2> <span>Actualités</span></h2>
+                    <ul class="informations-text">
+                        <li>Système d'alarme désactivé</li>
+                        <li>Nascop : nos offres évoluent, soutien de proximité avec notre nouveau chat</li>
+                    </ul>
                 </div>
+            </article>
 
+            <article>
                 <div class="informations-wrapper">
-                    <fieldset>
-                        <legend>Dernières consommations</legend>
-                        <ul class="informations-text">
-                            <li>Température moyennde la maison : 18°C</li>
-                            <li>Attention ! Température de la chambre : 20°C</li>
-                        </ul>
-                    </fieldset>
+                    <h2> <span> Dernières consommations </span> </h2>
+                    <ul class="informations-text">
+                        <li>Température moyennde la maison : 18°C</li>
+                        <li>Attention ! Température de la chambre : 20°C</li>
+                    </ul>
                 </div>
+            </article>
         </section>
 
 
     </body>
-
-    <footer>
-        <div id="coordonnees">
-            <p> Nascop </br> Région Parisienne </br> +33 6 43 52 09 38 </br> mail@nascop.com
-            </p>
-
-        <div class="bottom-bar">
-            Tous droits Réservés © 2017 | Nascop
-        </div>
-        <div id="copyright">
-            <p> Tous droits réservés 2017 - © NASCOP - Solution Domotique </p>
-
-        </div>
-
-    </footer>
 
 </html>
