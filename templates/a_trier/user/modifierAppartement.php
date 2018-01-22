62ff72405fd7f0de1f75acfc80a2b1d8b716e745
Quentin Cornier <quco10702@ma-3020-lx>
2018-01-22 11:38:09 +0100

    headers repo

--- templates/a_trier/user/modifierAppartement.php
+++ templates/a_trier/user/modifierAppartement.php
@@ -17,23 +17,10 @@
     </head>
 
     <body>
-        <div class="header">
-            <div id="logo">
-                <img src="../../design/images/logo_nascop.png" alt="Logo Nascop" />
-            </div>
-            <nav>
-                <ul>
-                    <li><a class="menu_effect" href="#"> Accueil </a> </li>
-                    <li><a class="menu_effect" href="#"> Statistiques </a> </li>
-                    <li><a class="menu_effect" href="#"> Appartements </a> </li>
-                    <li><a class="menu_effect" href="#"> Capteurs </a> </li>
-                    <li><a class="menu_effect menu-right" href="#"> Nous contacter </a> </li>
-                    <li><a class="menu_effect" href="#"> Mon profil </a> </li>
-                    <li><a class="menu_effect" href="#"> Langue </a> </li>
-                </ul>
-            </nav>
-        </div>
-
+        
+        <?php include '../../headers/headerUserConnected.php';?>
+        
+        
         <div id="conteneur" class="bckg">
 
             <div class="app row">
