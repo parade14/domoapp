6d1272426eb7d3bba1f81bf4143ba62cf60b54e4
parade14 <ppaul.bellamy@gmail.com>
2018-01-22 13:08:09 +0100

    "captors files added"

---
+++ web/html/user/capteur.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../../design/css/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../../design/css/ionicons/css/ionicons.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="../../design/css/capteur-style.css" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+        <title>Titre</title>
+    </head>
+
+    <body>
+        <div class="header">
+            <div id="logo">
+                <img src="../../design/images/logo_nascop.png" alt="Logo Nascop" />
+            </div>
+            <nav>
+                <ul>
+                    <li><a class="menu_effect" href="../Accueil.php"> Accueil </a> </li>
+                    <li><a class="menu_effect" href="#"> Statistiques </a> </li>
+                    <li><a class="menu_effect" href="modifierAppartement.php"> Appartements </a> </li>
+                    <li><a class="menu_effect" href="#"> Capteurs </a> </li>
+                    <li><a class="menu_effect menu-right" href="#"> Nous contacter </a> </li>
+                    <li><a class="menu_effect" href="#"> Mon profil </a> </li>
+                    <li><a class="menu_effect" href="#"> Langue </a> </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="main-wrapper">
+            <div class="icon-effect-1 icon-effect-1a">
+                <a href="#" class="icon"><i class="fa fa-lightbulb-o"></i></a>
+                <a href="#" class="icon"><i class="fa fa-thermometer-three-quarters"></i></a>
+                <a href="#" class="icon"><i class="ionicons ion-waterdrop"></i></a>
+                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+                <a href="#" class="icon"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
+                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+            </div>
+        </div>
+
+        <div class="main__menu2">
+            <ul>
+                <li class="small"><a href="#">Mes appartements</a></li>
+                <li class="small"><a href="#">Appartement 1</a></li>
+                <li class="small"><a href="#">Appartement 2</a></li>
+            </ul>
+        </div>
+
+        <div class="container-boxes">
+            <div class="box first">
+                <span class="icon-cont"><i class="fa fa-bed"></i></span>
+
+                <h3>Chambre 1</h3>
+
+                <ul class="hidden">
+                    <li>Lorem ipsum dolor</li>
+                    <li>Set amet consecuter</li>
+                    <li>Lorem ipsum dolor</li>
+                    <li>Set amet consecuter</li>
+                </ul>
+                <a class="expand"><span>17</span></a>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+        <script src="../../javascript/capteur.js"></script>
+
+    </body>
+</html>
+
