8065b710aff77d057ea9622bb8f387a61151e3ad
parade14 <ppaul.bellamy@gmail.com>
2018-01-23 22:53:32 +0100

    "captor files updated"

--- templates/a_trier/capteur/capteur.php
+++ templates/a_trier/capteur/capteur.php
@@ -3,8 +3,6 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="../../design/css/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../../design/css/ionicons/css/ionicons.min.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="../../../web/design/css/capteur-style.css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
@@ -14,26 +12,51 @@
     <body>
        <?php include '../../headers/headerUserConnected.php'; ?>
 
-        <div class="main-wrapper">
-            <div class="icon-effect-1 icon-effect-1a">
-                <a href="#" class="icon"><i class="fa fa-lightbulb-o"></i></a>
-                <a href="#" class="icon"><i class="fa fa-thermometer-three-quarters"></i></a>
-                <a href="#" class="icon"><i class="ionicons ion-waterdrop"></i></a>
-                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
-                <a href="#" class="icon"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
-                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
-                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
-                <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
-            </div>
-        </div>
+       <div class="add_captor hidden">
+           <div class="login-header">
+               <h2 class="login-header-text">Ajout d'un capteur</h2>
+               <i class="fa fa-times login-cross"></i>
+           </div>
 
-        <div class="main__menu2">
+           <form action="" class="captor-container">
+               <p><select id="select-captor">
+                       <option value="captor_id_1">captor_name_1</option>
+                       <option value="captor_id_2">captor_name_2</option>
+                       <option value="captor_id_3">captor_name_3</option>
+                   </select>
+               </p>
+               <p><select id="select-room">
+                       <option value="room_id_1">room_name_1</option>
+                       <option value="room_id_2">room_name_2</option>
+                       <option value="new_room">new_room</option>
+                   </select>
+               </p>
+               <p><input type="submit" value="CREER"></p>
+           </form>
+       </div>
+
+       <div class="main-wrapper">
+           <div class="icon-effect-1 icon-effect-1a">
+               <a href="#" class="icon"><i class="fa fa-lightbulb-o"></i></a>
+               <a href="#" class="icon"><i class="fa fa-thermometer-three-quarters"></i></a>
+               <a href="#" class="icon"><i class="ionicons ion-waterdrop"></i></a>
+               <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+               <a href="#" class="icon"><i class="fa fa-tachometer" aria-hidden="true"></i></a>
+               <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+               <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+               <a href="#" class="icon"><i class="ionicons ion-android-walk"></i></a>
+           </div>
+       </div>
+
+       <div class="main__menu2">
             <ul>
                 <li class="small"><a href="#">Mes appartements</a></li>
                 <li class="small"><a href="#">Appartement 1</a></li>
                 <li class="small"><a href="#">Appartement 2</a></li>
             </ul>
-        </div>
+       </div>
+
+       <button class="add popup-with-form btn-connect" href="#test-form"><i class="ionicons ion-plus-round"></i></button>
 
         <div class="container-boxes">
             <div class="box first">
@@ -52,7 +75,7 @@
         </div>
 
         <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-        <script src="../../javascript/capteur.js"></script>
+        <script src="../../../web/javascript/capteur.js"></script>
 
     </body>
 </html>
--- web/javascript/capteur.js
+++ web/javascript/capteur.js
@@ -1,5 +1,152 @@
+$(document).ready(function() {
 
-$('.box').click(function() {
-    $(this).toggleClass('selected');
+    /*$('.box').click(function() {
+        $(this).toggleClass('selected');
+
+    });
+    */
+
+    menu_translate();
+
+
+    $(".main__menu2 ul li:first-child").click(function() {
+        $(".main__menu2 ul li").toggleClass("small");
+
+        if ($(".main__menu2 ul li").hasClass("small")) {
+
+            menu_translate();
+
+        } else {
+            $(".main__menu2 ul li:not(:first-child)").css({"-webkit-transform": "", "-moz-transform": "", "-ms-transform": "", "-o-transform": "", "transform": "", "visibility": "visible"});
+        }
+    });
+
+
+    function menu_translate()
+    {
+        $(".main__menu2 ul li:not(:first-child)").each(function (index) {
+            var translation = (index + 1) * (-100);
+            $(this).css({"-webkit-transform": "translate("+translation+"%)", "-moz-transform": "translate("+translation+"%)", "-ms-transform": "translate("+translation+"%)", "-o-transform": "translate("+translation+"%)", "transform": "translate("+translation+"%)"});
+        });
+    }
+
+    $.fn.inlineEdit = function(replaceWith) {
+
+        $("body").on("click", "a.expand", function() {
+
+            var elem = $(this);
+
+            elem.hide();
+            elem.after(replaceWith);
+            replaceWith.focus();
+
+            replaceWith.blur(function() {
+
+                if ($(this).val() != "") {
+                    elem.text($(this).val());
+                }
+
+                $(this).remove();
+                elem.show();
+            });
+        });
+    };
+
+    var replaceWith = $('<input name="temp" class="temp" type="text" />');
+
+    $('a.expand').inlineEdit(replaceWith);
+
+    $('.btn-connect').click(function(){
+        if ($('div.add_captor').hasClass('hidden'))
+            $('div.add_captor').removeClass('hidden');
+        $('body > *:not(div.add_captor)').css({'-webkit-filter': 'blur(5px)'});
+        $('div.add_captor').animate({top: '35%'}, 700);
+        $.scrollLock( true );
+    });
+
+
+    $('.login-cross, input[type="submit"]').click(function(evt){
+        $('div.add_captor').css({'top': '0'});
+        $('body > *:not(div.add_captor)').css({'-webkit-filter': ''});
+        $('div.add_captor').addClass("hidden");
+        $.scrollLock( false );
+    });
+
+    $.scrollLock = ( function scrollLockSimple(){
+        var locked   = false;
+        var $body;
+        var previous;
+
+        function lock(){
+            if( !$body ){
+                $body = $( 'body' );
+            }
+
+            previous = $body.css( 'overflow' );
+
+            $body.css( 'overflow', 'hidden' );
+
+            locked = true;
+        }
+
+        function unlock(){
+            $body.css( 'overflow', previous );
+
+            locked = false;
+        }
+
+        return function scrollLock( on ) {
+            // If an argument is passed, lock or unlock depending on truthiness
+            if( arguments.length ) {
+                if( on ) {
+                    lock();
+                }
+                else {
+                    unlock();
+                }
+            }
+            // Otherwise, toggle
+            else {
+                if( locked ){
+                    unlock();
+                }
+                else {
+                    lock();
+                }
+            }
+        };
+    }() );
+
+    var Count = 1;
+
+    $(document).on('submit', '.captor-container', function() {
+        var form_content = '<div class="box first">' +
+            '                <span class="icon-cont"><i class="fa fa-bed"></i></span>' +
+            '                <h3>Chambre 1</h3>' +
+            '                <ul class="hidden">\n' +
+            '                    <li>Lorem ipsum dolor</li>' +
+            '                    <li>Set amet consecuter</li>' +
+            '                    <li>Lorem ipsum dolor</li>' +
+            '                    <li>Set amet consecuter</li>' +
+            '                </ul>' +
+            '                <a class="expand"><span>17</span></a>' +
+            '            </div>';
+        $(".box.first").last().after(form_content);
+
+        return false;
+
+    });
+
+    var room_input = $('<input name="room" class="room" type="text" placeholder="Nom de la nouvelle pièce" />');
+
+    $('#select-room').change(function() {
+        if ($(this).val() === 'new_room') {
+
+            var select_room = $(this);
+            select_room.hide();
+            select_room.after(room_input);
+            room_input.focus();
+        }
+    });
 
 });
